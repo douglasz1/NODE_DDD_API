a9efe60694f3a053953bfadf8dcd7e1dd2a1ab12
douglasz1 <douglas.santana.dev@gmail.com>
2021-01-12 15:20:15 -0300

    dior :fire:

--- src/infrastructure/UserRepository.ts
+++ src/infrastructure/UserRepository.ts
@@ -2,3 +2,11 @@ import IUserReadOnlyRepository from "@pbb/application/repositories/IUserReadOnly
 import User from "@pbb/domain/User";
 import { injectable } from "inversify";
 
+@injectable()
+export default  class UserRepository implements IUserReadOnlyRepository {
+
+    public async fetch(user: User): Promise<User> {
+        throw new Error("Method not implemented.");
+    }
+
+}
